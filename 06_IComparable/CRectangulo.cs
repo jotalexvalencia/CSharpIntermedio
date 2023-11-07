54fec4d696f705a0b6c43178300bb57432e14e6e
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-11-07 09:07:25 -0500

    IComparable: esta interfaz implementa el metodo CompareTo el cual retorna un entero así : 1 si somos mas grandes que el objeto comparado, -1 si somos mas pequeños que el objeto comparado, 0 si somos iguales que el objeto comparado.

---
+++ 06_IComparable/CRectangulo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06_IComparable
+{
+    class CRectangulo : IComparable
+    {
+        private double ancho;
+        private double alto;
+        private double area;
+
+        public CRectangulo(double pAncho, double pAlto)
+        {
+            ancho = pAncho;
+            alto = pAlto;
+            CalculaArea();
+        }
+
+        private void CalculaArea()
+        {
+            area = ancho * alto;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}.{1}]={2}",ancho,alto,area);
+        }
+        // Implementación de IComparable
+        public int CompareTo(object obj)
+        {
+            // Hacemos type cast con el objeto con el cual nos vamos a comparar
+            CRectangulo temp = (CRectangulo)obj;
+
+            // Si somos mas grandes que el objeto regresamos 1
+            if (area > temp.area)
+                return 1;
+            // Si somos mas pequeños que el objeto regresamos -1
+            if (area < temp.area)
+                return -1;
+            // Somos iguales al objeto regresamos 0
+            return 0;
+        }
+    }
+}
---
+++ 06_IComparable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06_IComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CRectangulo[] rects = new CRectangulo[5];
+            rects[0] = new CRectangulo(7, 5);
+            rects[1] = new CRectangulo(6, 4);
+            rects[2] = new CRectangulo(4, 3);
+            rects[3] = new CRectangulo(7, 6);
+            rects[4] = new CRectangulo(5, 7);
+
+            // Imprimimos arreglo original
+            foreach (CRectangulo r in rects)
+                Console.WriteLine(r);
+
+            Console.WriteLine("-------------------------");
+
+            // Hacemos uso de sort, el cual necesita la implementacion de IComparable
+            Array.Sort(rects);
+
+            // Imprimimos arreglo ordenado
+            foreach (CRectangulo r in rects)
+                Console.WriteLine(r);
+
+        }
+    }
+}
