158f57a9b4f7acdd4e43731a51fc6894d8157d79
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-11-09 09:53:48 -0500

    Sobrecarga Binarios: para suma de números imaginarios

---
+++ 13_SobrecargaBinarios/CImaginario.cs
@@ -0,0 +1,41 @@
+﻿namespace _13_SobrecargaBinarios
+{
+    class CImaginario
+    {
+        // Atributos
+        private double entero;
+        private double imaginario;
+
+        public CImaginario(double pEntero, double pImaginario) =>
+            (entero, imaginario) = (pEntero, pImaginario);
+
+        // Propiedades
+        public double Entero { get => entero; set => entero = value; }
+        public double Imaginario { get => imaginario; set => imaginario = value; }
+
+        // Para mostrar el numero imaginario
+        public override string ToString()
+        {
+            if (imaginario < 0)
+                return string.Format("({0}  {1}i)", entero, imaginario);
+            else
+                return string.Format("({0} + {1}i)", entero, imaginario);
+
+        }
+
+        // Sobrecarga de los operadores binarios
+        // a= i1 + i2
+        public static CImaginario operator +(CImaginario i1, CImaginario i2)
+        {
+            double re = 0;
+            double ri = 0;
+
+            re = i1.Entero + i2.Entero;
+            ri = i1.Imaginario + i2.Imaginario;
+
+            CImaginario temp = new CImaginario(re, ri);
+            return temp;
+        }
+
+    }
+}
---
+++ 13_SobrecargaBinarios/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _13_SobrecargaBinarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CImaginario im1 = new CImaginario(1, 2);
+            CImaginario im2 = new CImaginario(3, 4);
+            CImaginario imr;
+            // Hacemos la suma
+            imr = im1 + im2;
+            Console.WriteLine("{0}+{1}={2}", im1, im2, imr);
+        }
+    }
+}
