f174d5dee7d5aaf63490565bed764a068ab2388c
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-11-09 11:14:30 -0500

    Sobrecarga binarios 2: hacemos *,/,- y la + que ya teniamos para números imaginarios.

---
+++ 14_SobrecargaBinarios2/CImaginario.cs
@@ -0,0 +1,77 @@
+﻿namespace _14_SobrecargaBinarios2
+{
+    class CImaginario
+    {
+        // Atributos
+        private double entero;
+        private double imaginario;
+
+        public CImaginario(double pEntero, double pImaginario) =>
+            (entero, imaginario) = (pEntero, pImaginario);
+
+        // Propiedades
+        public double Entero { get => entero; set => entero = value; }
+        public double Imaginario { get => imaginario; set => imaginario = value; }
+
+        // Para mostrar el numero imaginario
+        public override string ToString()
+        {
+            if (imaginario < 0)
+                return string.Format("({0}  {1}i)", entero, imaginario);
+            else
+                return string.Format("({0} + {1}i)", entero, imaginario);
+
+        }
+
+        // Sobrecarga de los operadores binarios
+        // a= i1 + i2
+        public static CImaginario operator +(CImaginario i1, CImaginario i2)
+        {
+            double re = 0;
+            double ri = 0;
+
+            re = i1.Entero + i2.Entero;
+            ri = i1.Imaginario + i2.Imaginario;
+
+            CImaginario temp = new CImaginario(re, ri);
+            return temp;
+        }
+        public static CImaginario operator -(CImaginario i1, CImaginario i2)
+        {
+            double re = 0;
+            double ri = 0;
+
+            re = i1.Entero - i2.Entero;
+            ri = i1.Imaginario - i2.Imaginario;
+
+            CImaginario temp = new CImaginario(re, ri);
+            return temp;
+        }
+
+        public static CImaginario operator *(CImaginario i1, CImaginario i2)
+        {
+            double re = 0;
+            double ri = 0;
+
+            re = (i1.Entero * i2.Entero) - (i1.Imaginario * i2.Imaginario);
+            ri = (i1.Entero * i2.Imaginario) + (i1.Imaginario * i2.Entero);
+
+            CImaginario temp = new CImaginario(re, ri);
+            return temp;
+        }
+        public static CImaginario operator /(CImaginario i1, CImaginario i2)
+        {
+            double re = 0;
+            double ri = 0;
+
+            re = ((i1.Entero * i2.Entero) + (i1.Imaginario * i2.Imaginario))
+                / ((i2.Entero * i2.Entero) + (i2.Imaginario * i2.Imaginario));
+
+            ri = ((i1.Imaginario * i2.Entero) - (i1.Entero * i2.Imaginario))
+                / ((i2.Entero * i2.Entero) + (i2.Imaginario * i2.Imaginario));
+
+            CImaginario temp = new CImaginario(re, ri);
+            return temp;
+        }
+    }
+}
---
+++ 14_SobrecargaBinarios2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_SobrecargaBinarios2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CImaginario im1 = new CImaginario(1, 2);
+            CImaginario im2 = new CImaginario(3, 4);
+            CImaginario imr;
+            // Hacemos la suma
+            imr = im1 + im2;
+            Console.WriteLine("{0} + {1}={2}", im1, im2, imr);
+            // Hacemos la resta
+            imr = im1 - im2;
+            Console.WriteLine("{0} - {1}={2}", im1, im2, imr);
+            // Hacemos la multiplicacion
+            imr = im1 * im2;
+            Console.WriteLine("{0} * {1}={2}", im1, im2, imr);
+            // Hacemos la division
+            imr = im1 / im2;
+            Console.WriteLine("{0} / {1}={2}", im1, im2, imr);
+        }
+    }
+}
